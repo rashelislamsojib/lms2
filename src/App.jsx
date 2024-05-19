7b979362db1092c529ee5327a3d7395937cd565d
Rashel Islam Sojib <mdrashelislam86@gmail.com>
2024-05-19 17:32:21 +0600

    code
    push code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+
+function App() {
+
+  return (
+    <div className='parrent'>
+      <h1 >Hello Programmers</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/header.jsx
@@ -0,0 +1,10 @@
+import head  from "./assets/images/head.png"
+function Header() {
+  return (
+    <div className='parrent'>
+      <img src={head} alt="header thumbil" />
+    </div>
+  )
+}
+
+export default Header
